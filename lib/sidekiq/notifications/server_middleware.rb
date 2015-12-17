9d419f5f507dd08503e8de65ffbe86da5f2a4db5
Allen Madsen <blatyo@gmail.com>
2015-12-16 19:12:48 -0500

    Fix indentation

--- lib/sidekiq/notifications/server_middleware.rb
+++ lib/sidekiq/notifications/server_middleware.rb
@@ -4,7 +4,7 @@ module Sidekiq
   module Notifications
     class ServerMiddleware
       def call(worker, job, queue)
-         ActiveSupport::Notifications.instrument('app.worker.perform', title: "#{worker.class.to_s}#perform", job: job) do
+        ActiveSupport::Notifications.instrument('app.worker.perform', title: "#{worker.class.to_s}#perform", job: job) do
           yield
         end
       end
